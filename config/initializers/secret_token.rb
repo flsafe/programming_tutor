eb8047acc70b4dc0378ef8b8570f0252340a09af
Francisco Licea <francisco.licea@gmail.com>
2011-05-14 12:48:15 -0500

    Inital Add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rasberry::Application.config.secret_token = 'fdfcb4d61eac2996cece620ee92e6dac79d445bd5775f076ad804bc80beb95a5bcee695a1bb14d6d9ca61c046e141238dad0ce7911fc9e9985fee4a0f7c5a47d'
