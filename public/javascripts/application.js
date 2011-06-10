086d7f00df00279e3b8532892be27b43ea80497e
Francisco Licea <francisco.licea@gmail.com>
2011-06-10 17:15:58 -0500

    Move text editor js into the jquery read function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,37 +1,39 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-/* Login modal window */
-	$(document).ready(function() {
-		$("#login a").click(function(e){
-			e.preventDefault(); 
-			TINY.box.show({url:"/login.js", width: 400});
-		});
-	});
-	
-/* Initialize the CodeMirror text editor */
-if ($('#text_editor_form').length){
-  var textEditor = CodeMirror.fromTextArea(document.getElementById('text_editor'),
-      {mode: "clike",
-       lineNumbers: true,
-       matchBrackets: true,
-       indentUnit: 2,
-       tabMode: "indent"});
+$(document).ready(function() {
 
-  /* When grading a solution, hide the exercise, text editor, etc..*/
-  $('input[value="Submit Solution"]').click(function(){
-    $('#text_editor_form').hide();
-    $('#timer').hide();
-    $('#exercise_problem_text').hide();
-    $('#grade_sheet_wrapper').html("Grading...");
+  /* Login modal window */
+  $("#login a").click(function(e){
+    e.preventDefault(); 
+    TINY.box.show({url:"/login.js", width: 400});
   });
 
-  /* Hide/show the spinner gif when checking syntax or checking solution*/
-  $('#text_editor_form').bind('ajax:beforeSend', function(evt, xhr, settings){
-    $('#message').html("");
-    $('#spinner').show();
-  })
-  .bind('ajax:complete', function(){
-    $('#spinner').hide();
-  });
-}
+  /* Initialize the CodeMirror text editor */
+  if ($('#text_editor_form').length){
+    var textEditor = CodeMirror.fromTextArea(document.getElementById('text_editor'),
+        {mode: "clike",
+         lineNumbers: true,
+         matchBrackets: true,
+         indentUnit: 2,
+         tabMode: "indent"});
+
+    /* When grading a solution, hide the exercise and text editor */
+    $('input[value="Submit Solution"]').click(function(){
+      $('#text_editor_form').hide();
+      $('#timer').hide();
+      $('#exercise_problem_text').hide();
+      $('#grade_sheet_wrapper').html("Grading...");
+    });
+
+    /* Hide/show the spinner gif when checking syntax or checking solution*/
+    $('#text_editor_form').bind('ajax:beforeSend', function(evt, xhr, settings){
+      $('#message').html("");
+      $('#spinner').show();
+    })
+    .bind('ajax:complete', function(){
+      $('#spinner').hide();
+    });
+  }
+});
+
