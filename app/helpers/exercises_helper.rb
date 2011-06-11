7216c8cc4cfc4ed26718ee0190ac770ac9d06458
Francisco Licea <francisco.licea@gmail.com>
2011-06-10 19:53:52 -0500

    Avoid looking up grades for anonymous users

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,2 +1,11 @@
 module ExercisesHelper
+
+  def grade_for(user, opts)
+    unless user.anonymous?
+      exercise = opts[:on]
+      grades = GradeSheet.grades_for(user) 
+      return grades[exercise.id] || ""         
+    end
+    ""
+  end
 end
