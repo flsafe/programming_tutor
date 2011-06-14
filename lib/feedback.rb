fb62b22176b3572b7d953f54c7d080ace4e33c5a
Francisco Licea <francisco.licea@gmail.com>
2011-06-13 20:01:52 -0500

    Update the feedback model for the new more efficient unit tests

--- lib/feedback.rb
+++ lib/feedback.rb
@@ -3,6 +3,10 @@ class Feedback
   # Return a friendly feedback message
   # based on the grade sheet.
   def self.on(grade_sheet)
+    if grade_sheet.syntax_error?
+      return "Your solution doesn't compile!"
+    end
+
     if grade_sheet.unit_tests_failed?
       Rails.logger.error "An invalid grade sheet was produced:"
       grade_sheet.errors.full_messages.each {|msg| Rails.logger.error(msg)}
@@ -10,25 +14,13 @@ class Feedback
     end
 
     grade_sheet.unit_tests.each_pair do |test_name, result|
-      if result[:error]
-        return to_friendly_error_msg(result[:error])
+      if nil == result[:output]
+        return "I think this would crash at run time! Double check it" 
       elsif result[:output].strip.chomp != result[:expected].strip.chomp
         return to_friendly_msg(result)
       end
     end
     "This looks like it could work!"
-  end
-
-  # The unit test encountered an error,
-  # give some feedback.
-  def self.to_friendly_error_msg(error)
-    case error
-      when :compile_error then "Your solution doesn't compile!"
-      when :runtime_error then "I think this would crash at runtime."
-      when :timeout_error then "I think this goes into an infinite loop."
-      when :memory_error  then "I think this would run out of memory."
-      when :syscall_error then "You don't have to make that system call!"
-    end
   end
 
   # The unit test failed, give some feedback
