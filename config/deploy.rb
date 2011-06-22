4e043c790b3d8e02cbe14758ccdf51748a748afa
Francisco Licea <francisco.licea@gmail.com>
2011-06-21 20:58:53 -0500

    Bundle install with deployment flag and without dev and test gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,5 +58,5 @@ end
 after "deploy:update_code", :bundle_install
 desc "install the necessary prerequisites"
 task :bundle_install, :roles => :app do
-  run "cd #{release_path} && bundle install"
+  run "cd #{release_path} && bundle install --without development test --deployment"
 end
