a31b34177aaac25bc556d06ea204a7445964307b
Alex Peattie <alexpeattie@gmail.com>
2011-07-10 10:59:20 +0100

    Add registration success message, and redirect to login - Closes #4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:notice] = "Account registered!"
-      redirect_back_or_default account_url
+      redirect_to login_path(:ref => "register_success")
     else
       render :action => :new
     end
