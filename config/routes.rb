927da77dc5c41872be95b884be53cc20a3ec67ba
Francisco Licea <francisco.licea@gmail.com>
2011-07-14 11:43:18 -0500

    Add the 10KLOC badge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rasberry::Application.routes.draw do
 
+  resources :ten_thousand_lines_of_codes, :controller => 'badges'
   resources :five_thousand_lines_of_codes, :controller => 'badges'
   resources :one_thousand_lines_of_codes, :controller => 'badges'
   resources :the_good_student_badges, :controller => 'badges'
