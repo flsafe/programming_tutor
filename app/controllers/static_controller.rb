3662da977e359ecb8301f7c2033f1b05685b1f6e
Alex Peattie <alexpeattie@gmail.com>
2011-07-14 23:04:30 +0100

    Implement dropdown menu

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
   def index
     @lessons = Lesson.finished.order("created_at desc").limit(3)
     @leaders = User.leaders
+    @courses = current_user.admin? ? Course.all : Course.finished
   end
   
   def about
