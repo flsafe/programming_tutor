54784699247b7a5415307e58a82392e004bcda94
Francisco Licea <francisco.licea@gmail.com>
2011-07-31 15:12:36 -0500

    Switch to ActiveRecord session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rasberry::Application.config.session_store :cookie_store, :key => '_rasberry_session'
+#Rasberry::Application.config.session_store :cookie_store, :key => '_rasberry_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Rasberry::Application.config.session_store :active_record_store
+Rasberry::Application.config.session_store :active_record_store
